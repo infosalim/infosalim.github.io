e4796879a73208a6a2f168f25809ff53e00dc443
Md Salim Hossain <salimh5757@gmail.com>
2018-05-06 23:10:46 +0600

    updated

--- assets/js/active.js
+++ assets/js/active.js
@@ -4,18 +4,67 @@ $(document).ready(function() {
                     return $(this).attr("aria-valuenow") + "%";
                 }
         )
+
+
+    //jquery for scrollspy
+
+    var headerHeight = $(".header-area").outerHeight();
+    var scrollLink = $(".spy-btn");
+    // smooth scrolling
+
+    scrollLink.click(function(e){
+       e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        },800);
     });
+    //end scrollspy
+
+
+
+    $(window).scroll(function(){
+
+        //parallax
+        parallax();
+
+        //scrollspy active link switching
+        var scrollBarLocation = $(this).scrollTop();
+        scrollLink.each(function(){
+           var sectionOffset = $(this.hash).offset().top - 20
+
+           if( sectionOffset <= scrollBarLocation ){
+               $(this).addClass("active-class");
+               $(this).parent().siblings().find('a').removeClass("active-class");
+           }
+        });
+    });
+
+});
 
 /**********
               Parallax
  **********/
 
-$(window).scroll(function(){
-    parallax();
-});
 
 function parallax(){
     var wScroll = $(window).scrollTop();
-    
+
     $(".salim-intro, .salim-skill-area, .contact-area").css("background-position","center "+ (wScroll* -0.5)+"px");
 }
+
+/**********
+              scroll black
+**********/
+$(window).on('scroll',function(){
+    if($(window).scrollTop()){
+        $('.header-area').addClass('black');
+    }else{
+        $('.header-area').removeClass('black');
+    }
+});
+
+
+/**********
+              scrollspy
+**********/
